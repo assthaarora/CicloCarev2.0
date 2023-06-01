af9438bbc03645bd43cfa624d36f7bb9ea37fc26
Varinder <kumarvarinder1170@gmail.com>
2023-06-02 00:14:37 +0530

    merged head to master

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,58 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-<<<<<<< HEAD
-=======
 use Auth;
 use DB;
-
 use App\Models\OrderDetail;
 
->>>>>>> master
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-
-<<<<<<< HEAD
-    
-    public function index()
-    {
-        return view('home.index');
-=======
 
     public function index(){
         $patient = DB::table('patient_case')->where('userId', Auth::user()->id)->first();
         $orders = OrderDetail::where('patient_id', $patient->id)->orderBy('created_at', 'DESC')->get();
         dd($patient,$orders);
         return view('home.index', compact('patient', 'orders'));
->>>>>>> master
+
     }
     public function patient_dashboard(){
         return view('home.index');
-    }
-<<<<<<< HEAD
-    
-    public function order_management(){
-        return view('home.order_management');
-    }
-
-    
-=======
+     }
 
     public function order_management(){
         $patient = DB::table('patient_case')->where('userId', Auth::user()->id)->first();
@@ -63,5 +32,5 @@ class HomeController extends Controller
     }
 
 
->>>>>>> master
+
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,31 +6,24 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
         <div class="d-flex justify-content-center py-4">
           <a href="index.html" class="logo d-flex align-items-center w-auto">
             <img src="assets/img/logo.png" alt="">
             <span class="d-none d-lg-block">CicloCare</span>
           </a>
-        </div><!-- End Logo  -->
-
+        </div>
         <div class="card mb-3">
-          <!-- <div class="card-header">{{ __('Login') }}</div> -->
           <div class="card-body">
 
             <div class="pt-4 pb-2">
               <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
               <p class="text-center small">Enter your username &amp; password to login</p>
             </div>
-
             <form class="row g-3 needs-validation" novalidate="" method="POST" action="{{ route('login') }}">
             @csrf
               <div class="col-12">
                 <label for="yourUsername" class="form-label">{{ __('Email Address') }}</label>
                 <div class="input-group has-validation">
-                  <!-- <span class="input-group-text" id="inputGroupPrepend">@</span>
-                  <input type="text" name="username" class="form-control" id="yourUsername" required="">
-                  <div class="invalid-feedback">Please enter your username.</div> -->
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                   @error('email')
@@ -40,11 +33,8 @@
                   @enderror
                 </div>
               </div>
-
               <div class="col-12">
                 <label for="yourPassword" class="form-label">{{ __('Password') }}</label>
-                <!-- <input type="password" name="password" class="form-control" id="yourPassword" required="">
-                <div class="invalid-feedback">Please enter your password!</div> -->
                 <div class="col-md-12">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -55,7 +45,6 @@
                     @enderror
                 </div>
               </div>
-
               <div class="col-12">
                 <div class="form-check">
                   <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
@@ -66,23 +55,14 @@
                 <button class="btn btn-primary w-100" type="submit">{{ __('Login') }}</button>
               </div>
               <div class="col-12">
-<<<<<<< HEAD
-                <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
-=======
                 <p class="small mb-0">Don't have account? <a href="{{route('/')}}">Create an account</a></p>
->>>>>>> master
               </div>
             </form>
-
           </div>
         </div>
-
-       
-
       </div>
     </div>
   </div>
-
 </section>
 
 
